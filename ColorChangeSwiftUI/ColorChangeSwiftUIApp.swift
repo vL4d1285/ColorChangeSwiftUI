6643b3dd9cbfd383a536b4e61b0abe532cf4b73a
Vladislav Ryabtsev <vryabtsev@me.com>
2022-01-25 23:19:31 +0300

    Initial Commit

---
+++ ColorChangeSwiftUI/ColorChangeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangeSwiftUIApp.swift
+//  ColorChangeSwiftUI
+//
+//  Created by Vladislav Ryabtsev on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
